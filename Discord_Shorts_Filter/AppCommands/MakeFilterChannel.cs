87b15c3003e78a2526a1ae27578d65f9d72216af
Ryan Steffan <ryansteffan.biz@gmail.com>
2024-10-31 16:58:29 -0500

    Updated MakeFilterChannel.cs: Added the logic to handle creating a new discord channel.

--- Discord_Shorts_Filter/AppCommands/MakeFilterChannel.cs
+++ Discord_Shorts_Filter/AppCommands/MakeFilterChannel.cs
@@ -2,19 +2,24 @@
 using Discord.Net;
 using Discord.WebSocket;
 using Discord_Shorts_Filter.Logging;
+using Discord_Shorts_Filter.tools;
 using Newtonsoft.Json;
 
 namespace Discord_Shorts_Filter.AppCommands
 {
     internal static class MakeFilterChannel
     {
+ 
+        private static readonly string defaultChannelName = "filter_channel";
+        private static readonly string addChannelOptionName = "channel_name";
+        
         public static async Task AddCommand(DiscordSocketClient client)
         {
             SlashCommandBuilder command = new SlashCommandBuilder();
             
             command.WithName("make_filter_channel");
             command.WithDescription("Creates a channel that is used to filter out YouTube shorts.");
-            command.AddOption("channel_name", 
+            command.AddOption(addChannelOptionName, 
                               ApplicationCommandOptionType.String, 
                               "The name of the channel that will be created and use for filtering.\nDefaults to: \"filter_channel\"", 
                               isRequired: false);
@@ -32,7 +37,39 @@ namespace Discord_Shorts_Filter.AppCommands
 
         public static async Task CommandHandler(SocketSlashCommand command)
         {
-            await command.RespondAsync(text: "The filter channel has been created.", ephemeral: true);
+            if (command.GuildId == null)
+            {
+                await command.RespondAsync("This command must be used in a server!");
+            }
+            
+            SocketGuildChannel channel = (SocketGuildChannel)command.Channel;
+            SocketGuild guild = channel.Guild;
+            
+            IReadOnlyCollection<SocketSlashCommandDataOption> optionsReadOnly = command.Data.Options;
+            Dictionary<string, string?> optionsMap = new Dictionary<string, string?>();
+
+            foreach (SocketSlashCommandDataOption option in optionsReadOnly)
+            {
+                optionsMap.Add(option.Name, option.Value.ToString());
+            }
+
+            if (optionsMap.ContainsKey(addChannelOptionName))
+            {
+                string validChannelName = new ValidatedChannelName(optionsMap[addChannelOptionName]).ValidName;
+                await CreateFilterChannel(guild, validChannelName);
+            }
+            else
+            {
+                await CreateFilterChannel(guild, defaultChannelName);
+            }
+
+            await command.RespondAsync("Created filter channel!", ephemeral: true);
+        }
+
+        private static async Task CreateFilterChannel(SocketGuild guild, string channelName)
+        {
+            ITextChannel channel = await guild.CreateTextChannelAsync(channelName);
+            Logger.Info($"Created channel: {channelName} In Server: {guild.Name}");
         }
     }
 }
