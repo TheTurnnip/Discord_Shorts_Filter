99929663f593991b26064100f37b59d19d60a76c
Ryan Steffan <ryansteffan.biz@gmail.com>
2024-10-31 16:39:51 -0500

    Added ValidatedChannelName.cs: Class that represents a name that has been validated to work with discord.

---
+++ Discord_Shorts_Filter/tools/ValidatedChannelName.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Shorts_Filter.tools
+{
+    internal class ValidatedChannelName
+    {
+        public string ValidName { get; private set; }
+        public string UnValidatedName { get; private set; }
+
+        public ValidatedChannelName(string unvalidatedName)
+        {
+            if (unvalidatedName == null)
+            {
+                throw new ArgumentNullException(unvalidatedName, "A null channel name can not be validated!");
+            }
+
+            UnValidatedName = unvalidatedName;
+            ValidatedName(unvalidatedName);
+        }
+
+        private void ValidatedName(string originalName)
+        {
+            foreach (char letter in originalName)
+            {
+                if (letter == ' ')
+                {
+                    ValidName += "_";
+                }
+                else if (char.IsUpper(letter))
+                {
+                    ValidName += letter.ToString().ToLower();
+                }
+                else
+                {
+                    ValidName += letter;
+                }
+            } 
+        }
+    }
+}
